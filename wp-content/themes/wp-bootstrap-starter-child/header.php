34236f32dca01a4b1ffd442bd4670582480ad18a
Jonathan Morales <jonmorales22@gmail.com>
2018-02-14 18:18:59 -0500

    Website is starting to come along. Gonna fix up some redundancies in the css to clean things up before continuing on any major changes.

--- wp-content/themes/wp-bootstrap-starter-child/header.php
+++ wp-content/themes/wp-bootstrap-starter-child/header.php
@@ -12,11 +12,13 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet"> 
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
 <?php wp_head(); ?>
 </head>
 
--- wp-content/themes/wp-bootstrap-starter-child/page.php
+++ wp-content/themes/wp-bootstrap-starter-child/page.php
@@ -13,61 +13,70 @@
  */
 
 ?>
-	<?php wp_head();?>
-
-	<div class="bootstrap-child site-content">
-		<div class="bootstrap-child container">
-			<div class="bootstrap-child work row">
-
-	<section id="primary" class="bootstrap-child content-area col-sm-12 col-lg-8">
-		<main id="main" class="site-main" role="main">
-			<div class="profile-container">
-				<h1>Jonathan Morales. Coder. Musician. Life-Long Learner</h1>
-				<figure>
-					<div class="profile-pic-frame">
-						<img class="image-responsive profile-pic" src="<?php echo get_stylesheet_directory_uri();?>/assets/profile-pic.jpg"/>
-					</div>
-				</figure>
-			</div>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-	<div class="project-container">
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Work+Sans|Sarina" rel="stylesheet"> 
 		<?php
-		$postArgs = array(
-			'posts_per_page'   => 5,
-			'offset'           => 0,
-			'category'         => '',
-			'category_name'    => '',
-			'orderby'          => 'date',
-			'order'            => 'DESC',
-			'include'          => '',
-			'exclude'          => '',
-			'meta_key'         => '',
-			'meta_value'       => '',
-			'post_type'        => 'post',
-			'post_mime_type'   => '',
-			'post_parent'      => '',
-			'author'	   => '',
-			'author_name'	   => '',
-			'post_status'      => 'publish',
-			'suppress_filters' => true 
-		);
-			$postsArray = get_posts($postArgs);
-			foreach ($postsArray as $post)
-			 {
-					echo '<div class="project-element"><h2>' . apply_filters('the_title', $post->post_title) . '</h2>';
-					echo apply_filters('the_content', $post->post_content) ."</div>";
-			}
-			unset($val);
-
+			wp_head();
 		?>
+	</head>
 
-	</div>
+	<main id="main" class="site-main" role="main">
+		<div class="background"></div>
+		<body>
+			<div class="content-container">
+				<div class="row"> 
+					<div class ="title">Jon Morales</div>
+					<div class ="tagline">Coder. Musician. Lover of Learning.</div>
+				</div>
+				
+				<div class="row"> 
+					<figure>
+						<div class ="profile-pic-frame">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/profile-pic-edit.jpg"/>
+						</div>
+					</figure>
+				</div>
+			</div><!--content-container-->
+			<div class="about-container">
+				<div class="about-title"><h1>About Me</h1></div>
+				<div class="about-text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt sed diam quis congue. In id tellus quis purus aliquet pulvinar id non leo. Phasellus sit amet urna vestibulum, tincidunt diam sed, commodo lorem. Suspendisse sit amet libero et mi tempor aliquet. Pellentesque vel scelerisque lacus, quis euismod elit. Sed tempor dui urna, ut pulvinar ante lobortis quis. Sed nec ligula quis tellus ultricies consequat quis at elit. Vestibulum malesuada ornare massa, eu auctor nisl lacinia vitae. Nunc commodo in sapien vitae condimentum. Fusce ac purus at urna porttitor rhoncus eu non neque. Maecenas ultrices dolor tincidunt tellus pharetra, in consequat felis ornare. Suspendisse a posuere nibh. 
+				</div>
+			</div><!--about-container-->
+			<div class="project-container">
+				<div class="about-title"><h1>Projects</h1></div>
+				<?php
+					$args = array(
+						'post_type' => 'post',
+					);
+					$query = new WP_Query($args);
 
-				</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- #content -->
+					if($query->have_posts() ){
+						while($query->have_posts()){
+							$query->the_post();
+							echo "<div class='project-element'>";
+							echo "<div class='project-title'>"; the_title(); echo "</div>";
+							if(has_post_thumbnail())
+							{
+								echo "<div class='project-pic'>"; the_post_thumbnail(); echo "</div>";
+							}
+							echo "<div class='project-text'>"; the_content();  echo "</div>";
+							echo "</div>";
+						}
+						wp_reset_postdata();
+					}
+
+
+				?>
+			</div><!--projects-container-->
+			<div class="contact-container">
+				<div class="about-title"><h1>Contact</h1></div>
+				<div class="about-text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt sed diam quis congue. In id tellus quis purus aliquet pulvinar id non leo. Phasellus sit amet urna vestibulum, tincidunt diam sed, commodo lorem. Suspendisse sit amet libero et mi tempor aliquet. Pellentesque vel scelerisque lacus, quis euismod elit. Sed tempor dui urna, ut pulvinar ante lobortis quis. Sed nec ligula quis tellus ultricies consequat quis at elit. Vestibulum malesuada ornare massa, eu auctor nisl lacinia vitae. Nunc commodo in sapien vitae condimentum. Fusce ac purus at urna porttitor rhoncus eu non neque. Maecenas ultrices dolor tincidunt tellus pharetra, in consequat felis ornare. Suspendisse a posuere nibh. 
+				</div>
+			</div><!--contact-container-->
+		</body>
+	</main><!-- #main -->
 
 <?php
 get_sidebar();
