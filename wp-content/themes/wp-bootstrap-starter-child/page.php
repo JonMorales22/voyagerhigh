d4cb8397313ca6f0ed07b1c0e6cdd1d6c49e1fd7
Jonathan Morales <jonmorales22@gmail.com>
2018-02-15 03:03:13 -0500

    Its getting there. Still need to fix the css redundancies, but its def coming along.

--- wp-content/themes/wp-bootstrap-starter-child/page.php
+++ wp-content/themes/wp-bootstrap-starter-child/page.php
@@ -14,7 +14,8 @@
 
 ?>
 	<head>
-		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Work+Sans|Sarina" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa|Work+Sans|Bungee" rel="stylesheet"> 
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css"/>
 		<?php
 			wp_head();
 		?>
@@ -28,7 +29,6 @@
 					<div class ="title">Jon Morales</div>
 					<div class ="tagline">Coder. Musician. Lover of Learning.</div>
 				</div>
-				
 				<div class="row"> 
 					<figure>
 						<div class ="profile-pic-frame">
@@ -37,14 +37,31 @@
 					</figure>
 				</div>
 			</div><!--content-container-->
-			<div class="about-container">
-				<div class="about-title"><h1>About Me</h1></div>
-				<div class="about-text">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt sed diam quis congue. In id tellus quis purus aliquet pulvinar id non leo. Phasellus sit amet urna vestibulum, tincidunt diam sed, commodo lorem. Suspendisse sit amet libero et mi tempor aliquet. Pellentesque vel scelerisque lacus, quis euismod elit. Sed tempor dui urna, ut pulvinar ante lobortis quis. Sed nec ligula quis tellus ultricies consequat quis at elit. Vestibulum malesuada ornare massa, eu auctor nisl lacinia vitae. Nunc commodo in sapien vitae condimentum. Fusce ac purus at urna porttitor rhoncus eu non neque. Maecenas ultrices dolor tincidunt tellus pharetra, in consequat felis ornare. Suspendisse a posuere nibh. 
+			<div class="child-container">
+				<div class="about-title"><h1>About<h1></div>
+				<div class="project-element rounded">
+					<span class="oi oi-media-record"></span>
+					<h2>Hobbies</h2>
+					<p>Guitar, Bass Guitar, Piano, Cooking, Learning Languages, Reading</p>
+				</div>
+				<div class="project-element rounded">
+					<span class="oi oi-media-record"></span>
+					<h2>Skills</h2>
+					<p>C/C++/C#, Java, SQL, Perl, Prolog / LISP, JavaScript, Assembler, XML,
+					</p>
+				</div>
+				<div class="project-element rounded">
+					<span class="oi oi-media-record"></span>
+					<h2>Education</h2>
+					<p>East Stroudsburg University - 2013-2018</p>
+				</div>
+				<div class="project-element rounded">
+					<span class="oi oi-media-record"></span>
+					<h2>Resume</h2>
 				</div>
 			</div><!--about-container-->
-			<div class="project-container">
-				<div class="about-title"><h1>Projects</h1></div>
+			<div class="child-container">
+				<div class="container-title"><h1>Projects</h1></div>
 				<?php
 					$args = array(
 						'post_type' => 'post',
@@ -54,26 +71,44 @@
 					if($query->have_posts() ){
 						while($query->have_posts()){
 							$query->the_post();
-							echo "<div class='project-element'>";
-							echo "<div class='project-title'>"; the_title(); echo "</div>";
-							if(has_post_thumbnail())
-							{
-								echo "<div class='project-pic'>"; the_post_thumbnail(); echo "</div>";
-							}
-							echo "<div class='project-text'>"; the_content();  echo "</div>";
-							echo "</div>";
+							echo "<div class='project-element card'>";
+								echo "<div class='card-body'>";
+								echo "<div class='card-title'>"; the_title(); echo "</div>";
+								if(has_post_thumbnail())
+								{
+									$featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); 
+									echo "<img class='card-img-top' src='" . $featured_img_url . "'</div>";
+								}
+								echo "<p class='card-text'>"; the_content();  echo "</p>";
+								echo "</div>";//<----end card-body ---->
+							echo "</div>"; //<----end card ---->
 						}
 						wp_reset_postdata();
 					}
-
-
 				?>
 			</div><!--projects-container-->
-			<div class="contact-container">
-				<div class="about-title"><h1>Contact</h1></div>
-				<div class="about-text">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt sed diam quis congue. In id tellus quis purus aliquet pulvinar id non leo. Phasellus sit amet urna vestibulum, tincidunt diam sed, commodo lorem. Suspendisse sit amet libero et mi tempor aliquet. Pellentesque vel scelerisque lacus, quis euismod elit. Sed tempor dui urna, ut pulvinar ante lobortis quis. Sed nec ligula quis tellus ultricies consequat quis at elit. Vestibulum malesuada ornare massa, eu auctor nisl lacinia vitae. Nunc commodo in sapien vitae condimentum. Fusce ac purus at urna porttitor rhoncus eu non neque. Maecenas ultrices dolor tincidunt tellus pharetra, in consequat felis ornare. Suspendisse a posuere nibh. 
-				</div>
+			<div class="child-container">
+				<div class="form-container">
+					<div class="container-title"><h1>Contact</h1></div>
+					<form>
+					  <div class="form-group">
+					    <label for="exampleFormControlInput1">Your Name</label>
+					    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="John Smith">
+					  </div>
+					  	<div class="form-group">
+					    <label for="exampleFormControlInput1">Your Email</label>
+					    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleFormControlTextarea1">Message</label>
+					    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+					  </div>
+					  <div class="button-holder">
+						  <button type="button" class="btn btn-primary btn-sm">Clear</button>
+						  <button type="button" class="btn btn-primary btn-sm">Send</button>
+					  </div>
+					</form>
+				</div><!--form container-->
 			</div><!--contact-container-->
 		</body>
 	</main><!-- #main -->
